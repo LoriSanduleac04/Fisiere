08084d71e773aa54a22041091b9be5245eec35be
Loredana Sanduleac <70658391+LoriSanduleac04@users.noreply.github.com>
2021-12-08 17:40:18 +0200

    Update divizareaingrupe11C.py

--- divizareaingrupe11C.py
+++ divizareaingrupe11C.py
@@ -1,4 +1,4 @@
-with open("listaclasei.txt", "r") as f:
+with open("listaclasei.txt","r") as f:
 	linii = f.read()
 franceza=[]
 nf=[] 
@@ -6,15 +6,15 @@ engleza=[]
 ne=[] 
 for i in linii.split("\n"):
 	c=i.split()
-	if str(c[3])=='Engleza': ne.append(int(c[2])), engleza.append(c)
-	if str(c[3])=='Franceza': nf.append(int(c[2])), franceza.append(c)
-with open ("limbaengleza.txt", "w") as f: 
-	for i in range(0, len(engleza)):
-		for l in range(0, len(engleza[0])): f.write(str(engleza[i][l])+str(' '))
+	if str(c[3])=='Engleza':ne.append(int(c[2])),engleza.append(c)
+	if str(c[3])=='Franceza':nf.append(int(c[2])),franceza.append(c)
+with open ("limbaengleza.txt","w") as f: 
+	for i in range(0,len(engleza)):
+		for l in range(0,len(engleza[0])):f.write(str(engleza[i][l])+str(' '))
 		f.write("\n")
 	f.writelines("Media: "+ str(sum(ne)/len(ne)))
-with open ("limbafranceza.txt", "w") as f:
-	for i in range(0, len(franceza)): 
-		for j in range(0, len(franceza[0])): f.write(str(franceza[i][j])+str(' '))
+with open ("limbafranceza.txt","w") as f:
+	for i in range(0,len(franceza)): 
+		for j in range(0,len(franceza[0])):f.write(str(franceza[i][j])+str(' '))
 		f.write("\n")
 	f.writelines("Media: "+ str(sum(nf)/len(nf))) 
